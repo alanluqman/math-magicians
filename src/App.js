cfc9bc47cd8b9d825970415588ad0c04fd867d66
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-08-03 12:05:14 +0300

    change request  performed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Action, Buttons, Display } from './components/calculator';
+import { Calculator } from './components/calculator';
 import './components/calculator.css';
 
-const App = () => (
-  <div className="calculator">
-    <div>
-      <Display value={0} />
-    </div>
-    <div className="btnBoard">
-      <Buttons number="AC" />
-      <Buttons number="+/-" />
-      <Buttons number="%" />
-      <Action lable="/" />
-      <Buttons number={7} />
-      <Buttons number={8} />
-      <Buttons number={9} />
-      <Action lable="x" />
-      <Buttons number={4} />
-      <Buttons number={5} />
-      <Buttons number={6} />
-      <Action lable="-" />
-      <Buttons number={1} />
-      <Buttons number={2} />
-      <Buttons number={3} />
-      <Action lable="+" />
-      <div className="col-2-width">
-        <Buttons number={0} />
-      </div>
-      <Buttons number="." />
-      <Action lable="=" />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
+}
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -38,3 +38,40 @@ export class Action extends React.Component {
 Action.defaultProps = {
   lable: '?',
 };
+
+export class Calculator extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="calculator">
+          <div>
+            <Display value={0} />
+          </div>
+          <div className="btnBoard">
+            <Buttons number="AC" />
+            <Buttons number="+/-" />
+            <Buttons number="%" />
+            <Action lable="/" />
+            <Buttons number={7} />
+            <Buttons number={8} />
+            <Buttons number={9} />
+            <Action lable="x" />
+            <Buttons number={4} />
+            <Buttons number={5} />
+            <Buttons number={6} />
+            <Action lable="-" />
+            <Buttons number={1} />
+            <Buttons number={2} />
+            <Buttons number={3} />
+            <Action lable="+" />
+            <div className="col-2-width">
+              <Buttons number={0} />
+            </div>
+            <Buttons number="." />
+            <Action lable="=" />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
