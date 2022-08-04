1535dfe43b5acf9f0fb1a3a0d8f6f4bf3ab514b7
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-08-04 19:33:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,5 @@ import React from 'react';
 import Calculator from './components/calculator';
 import './components/calculator.css';
 
-// class App extends React.Component {
-//   render() {
-//     return <Calculator />;
-//   }
-// }
-
 const App = () => <Calculator />;
 export default App;
